ae37f0fd56a7d0c21d6be12e0bab4470fb715a9c
brnorl47 <brnorl47@gmail.com>
2022-08-28 15:48:23 +0300

    Seed Data Added

--- NLayer.Core/BaseEntity.cs
+++ NLayer.Core/BaseEntity.cs
@@ -6,7 +6,7 @@
 
         public DateTime CreatedDate { get; set; }
 
-        public DateTime UpdatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
 
     }
 }
--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NLayer.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NLayer.Repository
 {
@@ -24,10 +19,23 @@ namespace NLayer.Repository
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
-
-
-
-
+            modelBuilder.Entity<ProductFeature>().HasData(
+                new ProductFeature()
+                {
+                    Id = 1,
+                    Color = "Kırmızı",
+                    Height = 100,
+                    Width = 200,
+                    ProductId = 1
+                },
+                new ProductFeature()
+                {
+                    Id = 2,
+                    Color = "Mavi",
+                    Height = 300,
+                    Width = 500,
+                    ProductId = 2
+                });
 
             base.OnModelCreating(modelBuilder);
         }
